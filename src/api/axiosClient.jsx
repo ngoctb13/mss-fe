9fa4ae5f0c6241cf7f99707246de5f0c76f92c55
ngoctb13 <ngoctb136@gmail.com>
2024-03-11 17:37:42 +0700

    update cors

--- src/api/axiosClient.jsx
+++ src/api/axiosClient.jsx
@@ -2,8 +2,8 @@ import axios from "axios";
 import queryString from "query-string";
 import { ACCESS_TOKEN } from "../constant/constant";
 const axiosClient = axios.create({
-  // baseURL: "http://167.172.77.23:8080/api",
-  baseURL: "http://localhost:8080/api",
+  baseURL: "http://167.172.77.23:8080/api",
+  // baseURL: "http://localhost:8080/api",
   headers: {
     "content-type": "application/json",
   },
