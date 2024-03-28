20328057a37acb1c3d81aa0a018e8fe18943194d
ngoctb13 <ngoctb136@gmail.com>
2024-03-28 23:16:36 +0700

    fix

--- src/constant/constant.jsx
+++ src/constant/constant.jsx
@@ -1,4 +1,5 @@
-export const API_BASE_URL = "http://localhost:8080";
+// export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = "https://backend.mss.io.vn";
 export const ACCESS_TOKEN = "accessToken";
 export const USER_ROLE = "userRole";
 export const TOKEN_EXPITY_TIME = 86400000; // 1 ngày
